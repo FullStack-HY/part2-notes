fe946973afff0fd6eaf60b344bb26c6f85f560ff
Matti Luukkainen <mluukkai@iki.fi>
2021-01-16 17:45:42 +0200

    part2-3

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -15,7 +14,7 @@ const App = (props) => {
       important: Math.random() > 0.5,
       id: notes.length + 1,
     }
-    
+
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
@@ -25,11 +24,20 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
             <Note key={note.id} note={note} />
         )}
       </ul>
