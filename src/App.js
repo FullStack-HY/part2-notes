84e4e2003ab637a4a3c64b711f8faf26e70c0402
Matti Luukkainen <mluukkai@iki.fi>
2021-01-17 16:13:31 +0200

    part2-8

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
+import Footer from './components/Footer'
 import noteService from './services/notes'
 
 const App = () => {
@@ -86,6 +87,7 @@ const App = () => {
         />
         <button type="submit">save</button>
       </form>  
+      <Footer />
     </div>
   )
 }
