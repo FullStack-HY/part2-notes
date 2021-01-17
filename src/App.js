f4bd46cf8e0494753608e58df7b9276f7539af12
Matti Luukkainen <mluukkai@iki.fi>
2021-01-17 11:55:15 +0200

    part2-5

--- src/App.js
+++ src/App.js
@@ -24,11 +24,14 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
 
   const handleNoteChange = (event) => {
