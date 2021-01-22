03756798cecb372142e820ba07c23c48b29b5d76
Matti Luukkainen <mluukkai@iki.fi>
2021-01-22 16:14:40 +0200

    part3-1

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,20 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
-
-const getAll2 = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    content: 'This note is not saved to server',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  }
-  return request.then(response => response.data.concat(nonExisting))
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
