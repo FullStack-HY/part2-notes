fb1b06c55a9722105303654b6709c5b3e3c7ce4c
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-04-09 07:51:07 -0400

    part2.1

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const Part = ({part}) => {
 const Content = ({ parts }) => {
   return (
     <div>
-      {parts.map(part => <Part part={part} />)}
+      {parts.map(part => <Part key={part.id} part={part} />)}
     </div>
   )
 }
