099fb3bae67212b83216680410330aa1311ad9ca
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-04-09 08:06:21 -0400

    part 2.3

--- src/index.js
+++ src/index.js
@@ -73,34 +73,4 @@ const App = () => {
   return <Course course={course} />
 }
 
-/*
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
-*/
-
 ReactDOM.render(<App />, document.getElementById('root'))
