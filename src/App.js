a118915fefdbce0674629ddf34ae17a1200854aa
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-04-13 18:40:34 -0400

    part 2.6

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
-import React from 'react';
-import Course from './components/Course'
+import React, { useState } from 'react'
 
 const App = () => {
-    const courses = [
-      {
-        name: 'Half Stack application development',
-        id: 1,
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          },
-          {
-            name: 'Redux',
-            exercises: 11,
-            id: 4
-          }
-        ]
-      }, 
-      {
-        name: 'Node.js',
-        id: 2,
-        parts: [
-          {
-            name: 'Routing',
-            exercises: 3,
-            id: 1
-          },
-          {
-            name: 'Middlewares',
-            exercises: 7,
-            id: 2
-          }
-        ]
-      }
-    ]
-  
-    return <Course courses={courses} />
+  const [ persons, setPersons ] = useState([
+    { id: 1,
+      name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const nameHandler = (event) => {
+    event.preventDefault()
+    let nameSubmit = {
+      id: persons.length + 1,
+      name: newName
+    }
+    setPersons(persons.concat(nameSubmit))
+    setNewName('')
   }
 
-  export default App
+  const nameChangeHandler = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={nameHandler}>
+        <div>
+          name:
+          <input value={newName} onChange={nameChangeHandler}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+        <li key={person.id}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
+import ReactDOM from 'react-dom'
+import App from './App.js'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
